02045b5c9009aaff6360d90c3e18db28e33f8a9b
ThomasTorres2024 <thomastorres2024@gmail.com>
2025-12-26 02:41:14 -0500

    p6 p2 done finally cuz im a bot

--- cpp_src/Day6/main.cpp
+++ cpp_src/Day6/main.cpp
@@ -112,6 +112,7 @@ void problem1(int agrv, char *argv[])
 
     dataFile.close();
 }
+
 /**
  * @brief Does an O(n) search over each bank looking for the max, and then the second max. Once these are found, we have enough info
  * for the joltage of each bank, and the total joltage overall, which is our ans.
@@ -128,7 +129,8 @@ void problem2(int agrv, char *argv[])
     // read in data in file as a large line
     std::string line;
 
-    std::vector<std::vector<std::string>> vals; 
+    std::vector<std::vector<std::string>> vals;
+    std::vector<std::string> data;
     std::vector<size_t> max;
     std::vector<std::string> ops;
 
@@ -137,12 +139,11 @@ void problem2(int agrv, char *argv[])
     while (std::getline(dataFile, line))
     {
 
+        // add to data matrix and get matrix of numeric vals
         std::istringstream sstream(line);
-
-        // std::cout << line << "\n";
+        data.push_back(line);
 
         std::string s;
-
         std::vector<std::string> curr;
 
         while (sstream >> s)
@@ -164,46 +165,104 @@ void problem2(int agrv, char *argv[])
         }
     }
 
-    // do iteration
-    size_t col = 0;
+    // obtain col-wise max
+    std::vector<size_t> maxes;
 
-    size_t total = 0;
-    for (auto op : ops)
+    // exterior loop for iterating through each col
+    for (size_t j = 0; j < vals.at(0).size(); j++)
     {
-
-        std::vector<std::vector<size_t>> toCut;
-
-        // std::cout<<vals.size()-1<<"\n";
+        size_t currMax = 0;
+        // go row wise now
         for (size_t i = 0; i < vals.size() - 1; i++)
         {
-            if(vals.at(i).at(col).size()<toCut.size())
-            toCut.push_back(vals.at(i).at(col));
+
+            // get new max idx
+            if (vals.at(i).at(j).size() > currMax)
+            {
+                currMax = vals.at(i).at(j).size();
+            }
         }
+        maxes.push_back(currMax);
+    }
+
+    size_t prev = 0;
+
+    std::vector<std::vector<std::string>> rowVals;
+
+    // now for each col parse using
+    //  exterior loop for iterating through each col
+    for (size_t j = 0; j < ops.size(); j++)
+    {
+        // std::cout << j << "\n";
+        size_t currMax = maxes.at(j);
+        // go row wise now
+
+        std::vector<std::string> rowNums(currMax, "");
+
+        // values over the cols
+        for (size_t k = 0; k < currMax; k++)
+        {
+            // values for each row
+            for (size_t i = 0; i < vals.size() - 1; i++)
+            {
+
+                // std::cout << "curr max: " << currMax << "\n";
+                // std::cout << data.at(i).at(prev + k) << "\n";
+                // std::cout << i << "," << prev + k << "\n";
+                // std::cout << "------------\n";
+
+                if (data.at(i).at(prev + k) != ' ')
+                {
+                    rowNums.at(k) += data.at(i).at(prev + k);
+                }
+            }
+        }
+
+        // for(auto r : rowNums){
+        //     std::cout<<r<<",";
+        // }
+        // std::cout<<"\n";
+
+        // increment next starting position for the cols
+        prev += currMax + 1;
+        rowVals.push_back(rowNums);
+    }
+
+    size_t total = 0;
+    for (size_t i = 0; i < ops.size(); i++)
+    {
+        std::string op = ops.at(i);
 
         if (op == "+")
         {
-            // std::cout<<vals.size()-1<<"\n";
-            for (size_t i = 0; i < vals.size() - 1; i++)
+            size_t subtotal = 0;
+            for (auto v : rowVals.at(i))
             {
-                total += vals.at(i).at(col);
-                // std::cout << vals.at(i).at(col) <<"\n";
+                subtotal += std::stoul(v);
             }
+            total+=subtotal;
         }
         else
         {
-            // std::cout<<vals.size()-1<<"\n";
-            size_t temp = 1;
-            for (size_t i = 0; i < vals.size() - 1; i++)
+            size_t subtotal = 1;
+            for (auto v : rowVals.at(i))
             {
-                temp *= vals.at(i).at(col);
-                // std::cout << vals.at(i).at(col) <<"\n";
+                subtotal *= std::stoul(v);
             }
-            total += temp;
+            total+=subtotal;
         }
-        col++;
     }
 
-    std::cout << "Total of Cephalapod Math: " << total << "\n";
+    // for (auto v : rowVals)
+    // {
+    //     for (auto a : v)
+    //     {
+    //         std::cout << a << ",";
+    //     }
+    //     std::cout << "\n";
+    // }
+
+    std::cout << "Total of Cephalapod Math P2: " << total << "\n";
 
     dataFile.close();
 }
